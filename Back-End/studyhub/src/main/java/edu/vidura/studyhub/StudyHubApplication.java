f2c86d1a101c62619b46233fd7fbe913120cce76
Vidura Priyadarshana <vidura200427@gmail.com>
2025-07-18 18:01:58 +0530

    Initial Commit

---
+++ Back-End/studyhub/src/main/java/edu/vidura/studyhub/StudyHubApplication.java
@@ -0,0 +1,11 @@
+package edu.vidura.studyhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyHubApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(StudyHubApplication.class, args);
+	}
+}
